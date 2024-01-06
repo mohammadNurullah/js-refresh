355c99f94982f18aab6da217de0518e892da4fea
mohammadNurullah <nurullahsarkar963@gmail.com>
2024-01-06 14:54:55 +0600

    lexical scope

--- refresh.js
+++ refresh.js
@@ -1,1 +1,14 @@
-alert("hello");
+//--LEXICAL SCOPE--//
+var name = "javascript";
+function myFunction(topic) {
+  var name = topic; // var function scope
+  //console.log(name);
+
+  if (true) {
+    let a = 41; // const & let block scope
+    console.log(a);
+  }
+  //console.log(a); // will get error
+}
+myFunction("python");
+//console.log(name);
