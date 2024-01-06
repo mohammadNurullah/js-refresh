2cc1f4f92e6b45dc23c9c90ed7ff7c982e5d3823
mohammadNurullah <nurullahsarkar963@gmail.com>
2024-01-06 15:42:05 +0600

    premitive and reference value

--- refresh.js
+++ refresh.js
@@ -6,9 +6,9 @@ function myFunction(topic) {
 
   if (true) {
     let a = 41; // const & let block scope
-    console.log(a);
+    // console.log(a);
   }
-  //console.log(a); // will get error
+  // console.log(a); // will get error
 }
 myFunction("python");
 //console.log(name);
@@ -19,25 +19,25 @@ myFunction("python");
 function regular() {
   console.log("Hello World! from regular");
 }
-regular();
+//regular();
 
 // Function Expression
 const hello = function () {
   console.log("Hello World! from expression");
 };
-hello();
+// hello();
 
 // Named Function Expression
 const namedFunc = function test() {
   console.log("Hello World! from Named function");
 };
-hello();
+// hello();
 
 // Arrow Function
 const arrowFunc = () => {
   console.log("Hello World! from Arrow function");
 };
-arrowFunc();
+// arrowFunc();
 
 // Anonymus Function
 const arwFn = () => {
@@ -46,4 +46,25 @@ const arwFn = () => {
   };
 };
 const res = arwFn();
-res();
+// res();
+
+//--PREMITIVE & REFERENCE VALUE--//
+
+// Premitive type value
+let x = 1;
+let y = 2;
+x = y;
+y = 3;
+
+// console.log(x);
+// console.log(y);
+
+// Reference type value
+
+let arr1 = ["Js", "python"];
+let arr2 = ["java", "cpp"];
+
+arr2 = arr1;
+arr1.push("Go");
+console.log(arr1);
+console.log(arr2);
